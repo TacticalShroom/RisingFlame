b8bbc8df02abb2d71e38b1811f88e12a95bf0130
TacticalShroom <gabrielschallock@icloud.com>
2022-05-04 20:30:07 -0700

    init

---
+++ src/RisingFlame.java
@@ -0,0 +1,29 @@
+import processing.core.PApplet;
+
+public class RisingFlame extends PApplet {
+
+
+
+
+
+
+    public void draw()  {
+
+    }
+
+    public void setup() {
+
+    }
+
+
+
+
+
+    public void settings()  {
+
+    }
+
+    public static void main(String[] args)  {
+        main("RisingFlame");
+    }
+}
